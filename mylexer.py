d6b6ba52d3756d683eae535ecc774f01a67785f3
Diego Pereira Cortinhas <diego.cortinhas@gmail.com>
2021-05-18 21:03:50 -0300

    Versão final implementada no arquivo mylexer.ply

--- mylexer.py
+++ mylexer.py
@@ -36,26 +36,24 @@ t_LOG = r'\&'
 
 
 
-# A regular expression rule with some action code
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
 
-# Define a rule so we can track line numbers
+# Definir os numeros das linhas
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 
-# A string containing ignored characters (spaces and tabs)
+# Funcao para ignorar espaços
 t_ignore = ' \t'
-
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Caractere não suportado pela gramatica. '%s'" % t.value[0])
     t.lexer.skip(1)
 
 # Build the lexer
